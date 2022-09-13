39fc940e12bcd8773aea2dc297f8302ca6986296
Ashraful Alam <mdmashraful1998@gmail.com>
2022-09-13 16:12:35 +0600

    conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,76 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
-import Book from './components/Book'
-
-
+import Book from './components/Book';
 
 class App extends Component {
-
   state = {
     books: [
-      { id: 1, bookName: "The Alchemist", Writer: "Poulo Koulho" },
-      { id: 2, bookName: "Jobon Jekhane Jemon", Writer: "Mizanur Rahman" },
-      { id: 3, bookName: "Thinking Fast and Slow", Writer: "Dan Brown" },
-      { id: 4, bookName: "How to grow Rich", Writer: "Anderw hardy" }
-    ]
-
+      { id: 1, bookName: "1984", writer: "George Orwell" },
+      { id: 2, bookName: "The Da Vinci Code", writer: "Dan Brown" },
+      { id: 3, bookName: "The Alchemist", writer: "Paulo Coelho" }
+    ],
+    showBooks: true
   }
 
+  changeWithInputState = (event, index) => {
+    const book = {
+      ...this.state.books[index]
+    }
+    book.bookName = event.target.value;
+    const books = [...this.state.books];
+    books[index] = book;
 
+    this.setState({ books: books });
+  }
 
   deleteBookState = index => {
-    const books = this.state.books;
+    //const books = this.state.books.slice();
+    //const books = this.state.books.map(item => item);
+    const books = [...this.state.books];
     books.splice(index, 1);
     this.setState({
       books: books
-    })
+    });
   };
 
+  toggleBooks = () => {
+    this.setState({ showBooks: !this.state.showBooks });
+  }
 
-
-
-
-
-  //changing state with javaScript event
-  // constructor() {
-  //   super()
-  //   this.state();
-  // }
   render() {
-    const Style = {
-      border: "1px red",
+    const style = {
+      border: "1px solid red",
       borderRadius: "5px",
-      background: "black",
-      color: "white"
+      backgroundColor: "black",
+      color: "white",
     };
 
-    const booksState = this.state.books;
+    //const booksState = this.state.books;
 
-    const books = booksState.map((book, index) => {
+    const books = this.state.books.map((book, index) => {
       return (
         <Book
           bookName={book.bookName}
-          Writer={book.Writer}
+          writer={book.writer}
           delete={() => this.deleteBookState(index)}
           key={book.id}
-
+          inputName={(event) => this.changeWithInputState(event, index)}
         />
       );
     });
 
     //console.log(booksState);
-
-
-
+    console.log(books);
 
     return (
-      <div className='App'>
-        <h1 style={Style}>Book List</h1>
-        {books}
+      <div className="App">
+        <h1 style={style}>Book List</h1>
+        <button onClick={this.toggleBooks}>Toggle Books</button>
+        {this.state.showBooks ? books : null}
       </div>
     );
   }
 }
-
 
 export default App;
