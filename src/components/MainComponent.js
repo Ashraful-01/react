fac01fb7cdc42d5393ced1eec0359f8780599ee8
Ashraful Alam <mdmashraful1998@gmail.com>
2022-09-26 01:28:52 +0600

    adding redirect

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,7 @@ import BookList from './lists/BookList';
 import booklist from '../assets/books';
 import NewBook from './representationalComponent/NewBook';
 import BookDetails from './representationalComponent/BookDetails';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, redirect } from 'react-router-dom';
 
 
 
@@ -41,14 +41,15 @@ class MainComponent extends Component {
 
                 <nav className="nav-bar">
                     <ul>
-                        <li><NavLink to="/" exact>Home</NavLink></li>
+                        <li><NavLink to="/books" exact>Home</NavLink></li>
                         <li><NavLink to="/new-book">New Book</NavLink></li>
                     </ul>
                 </nav>
                 <Switch>
-                    <Route path="/" exact render={() => books} />
+                    <Route path="/books" exact render={() => books} />
                     <Route path="/new-book" exact Component={NewBook} />
                     <Route path="/book:id" render={() => <BookDetails book={this.state.selectedBook} />} />
+                    <redirect  from="/" to="/books"/>
                 </Switch>
             </div>
 
